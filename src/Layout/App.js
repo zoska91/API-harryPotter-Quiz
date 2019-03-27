8afa05101121e1503f9363b2e1a3d2747517bea6
zoska91 <zoska91@wp.pl>
2019-03-27 17:30:38 +0100

    update router

--- src/Layout/App.js
+++ src/Layout/App.js
@@ -1,74 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../style/App.css';
-import Header from './Header';
-import Section from './Section';
-import Footer from './Footer';
-import { BrowserRouter as Router } from 'react-router-dom'
+import "../style/App.css";
+import Header from "./Header";
+import Section from "./Section";
+import Footer from "./Footer";
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-const key = '$2a$10$tE9Q/PpSuP7rQLFkrB2IOOcl.0ptM34qLwotYCBjL/p9DIL.o4pMK';
-const url = new URL('https://www.potterapi.com/v1/characters/');
-const params = { key: key, }
+const key = "$2a$10$tE9Q/PpSuP7rQLFkrB2IOOcl.0ptM34qLwotYCBjL/p9DIL.o4pMK";
+const url = new URL("https://www.potterapi.com/v1/characters/");
+const params = { key: key };
 
 // niestety tego nie rozumiem, znalazłam w internecie
-Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
 class App extends Component {
-
   state = {
-    characters: [],
-  }
-
+    characters: []
+  };
 
   componentDidMount() {
     fetch(url, { key: key })
       .then(response => {
         if (response.ok) {
-          return response
+          return response;
         }
-        throw Error(response.status)
+        throw Error(response.status);
       })
-      .then((resp) => resp.json())
-      .then((data) => {
+      .then(resp => resp.json())
+      .then(data => {
         this.setState({
           characters: data
-        })
+        });
       })
-      .catch(function (error) {
+      .catch(function(error) {
         // Error handling
         console.log("There's an error with the API.");
       });
-
   }
 
-
   render() {
-
-    const { characters } = this.state
+    const { characters } = this.state;
 
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="app">
-
-          <header>
-            {<Header />}
-          </header>
+          <header>{<Header />}</header>
 
           <main>
-            <section>
-              {<Section characters={characters} />}
-            </section>
+            <section>{<Section characters={characters} />}</section>
           </main>
 
-          <footer>
-            {<Footer />}
-          </footer>
-
+          <footer>{<Footer />}</footer>
         </div>
-
       </Router>
-
     );
   }
 }
