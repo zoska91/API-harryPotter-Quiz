e530678598045a9e218cfb8b83b2537beb3de195
zoska91 <zoska91@wp.pl>
2019-03-27 17:18:11 +0100

    Updates

---
+++ precache-manifest.aad58b757f5a793b73e57c79053a403a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "218344254686e6e48551bcb4cdb54529",
+    "url": "/API-harryPotter-Quiz/static/media/r.21834425.png"
+  },
+  {
+    "revision": "804a0db5aa36c83dc403",
+    "url": "/API-harryPotter-Quiz/static/css/main.0d4c6ed8.chunk.css"
+  },
+  {
+    "revision": "128dcb81b6a7e880b5a1",
+    "url": "/API-harryPotter-Quiz/static/js/runtime~main.128dcb81.js"
+  },
+  {
+    "revision": "1f71d86224a051be0820",
+    "url": "/API-harryPotter-Quiz/static/js/2.1f71d862.chunk.js"
+  },
+  {
+    "revision": "c041e1a5cda2c6eca6365a3c3f4b0508",
+    "url": "/API-harryPotter-Quiz/static/media/g.c041e1a5.png"
+  },
+  {
+    "revision": "f65d90c8dd39522192a74e87ddc17140",
+    "url": "/API-harryPotter-Quiz/static/media/h.f65d90c8.png"
+  },
+  {
+    "revision": "804a0db5aa36c83dc403",
+    "url": "/API-harryPotter-Quiz/static/js/main.804a0db5.chunk.js"
+  },
+  {
+    "revision": "0e32fd1bbe327682795642083cb53b58",
+    "url": "/API-harryPotter-Quiz/static/media/s.0e32fd1b.png"
+  },
+  {
+    "revision": "db5150bea052ea4f619e6fc94e38aa94",
+    "url": "/API-harryPotter-Quiz/static/media/Harry-Potter-i-Insygnia-Smierci-Czesc-2.db5150be.jpg"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/API-harryPotter-Quiz/static/media/HARRYP__.181ef9a7.TTF"
+  },
+  {
+    "revision": "80a882977e0ba18ce65a5edd4e3f7ab3",
+    "url": "/API-harryPotter-Quiz/static/media/771px-DHhogwarts.80a88297.jpg"
+  },
+  {
+    "revision": "80f6d244009b7c551e0c630783360c46",
+    "url": "/API-harryPotter-Quiz/static/media/LumosLatino.80f6d244.ttf"
+  },
+  {
+    "revision": "1556b9bd402c60675a1acfa668cbfa46",
+    "url": "/API-harryPotter-Quiz/index.html"
+  }
+];
